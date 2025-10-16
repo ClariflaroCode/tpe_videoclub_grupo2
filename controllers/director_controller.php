019f3eb3a962ff8da61b4910d52f84a6477b9c83
juli <julietawatts.ar@gmail.com>
2025-10-15 22:52:43 -0300

    agrega mvc director

---
+++ controllers/director_controller.php
@@ -0,0 +1,46 @@
+<?php
+   require_once 'director_model.php';
+   require_once 'director_view.php';
+
+
+   class DirectorController {
+       private $model;
+       private $view;
+
+
+       function __construct() {
+           $this->model = new DirectorModel();
+           $this->view = new DirectorView();
+       }
+       public function addDirector($nombre, $sexo, $reputacion, $nacimiento, $pais ) {
+           if (!empty($nombre) && isset($nombre)) {
+               if (!empty($sexo) && isset($sexo)) {
+                   if (!empty($nombre) && isset($nombre)) {
+                      
+                   } else {
+                       $this->view->error("Falta el nombre");
+                   }
+               } else {
+                   $this->view->error("Falta el sexo del director");
+               }
+           } else {
+               $this->view->error("Falta el nombre");
+           }
+       }
+       public function editDirector($id, $nombre, $sexo, $reputacion, $nacimiento, $pais ) {
+
+
+       }
+       public function deleteDirector($id){
+
+
+       }
+       public function showDirectors() {
+
+
+       }
+   }   
+
+
+  
+?>
